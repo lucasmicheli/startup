a49fdb85600367e2ec20d2056cf77189e36f9649
Lucas Micheli <lucasmicheli@outlook.com>
2020-03-30 23:25:18 -0300

    Exercise 2 fixed

--- 04-HTML5-APIs/2- Drag and Drop/js/script.js
+++ 04-HTML5-APIs/2- Drag and Drop/js/script.js
@@ -1,57 +1,29 @@
 // Exercise 2
 
-const files = document.getElementById('files-area');
+const filesArea = document.getElementsByClassName('files-area')[0];
+const fileData = document.getElementsByClassName('file-content')[0];
 
-const removeData = (event) => {
-    if (event.dataTransfer.items) {
-        event.dataTransfer.items.clear();
-    } else {
-        event.dataTransfer.clearData();
-    }
-    console.log('Data removed.')
-};
-
-const listFiles = (file, list) => {
-    const ul = document.createElement('ul');
-    const fileName = document.createElement('li');
-    const fileType = document.createElement('li');
-    ul.setAttribute("class", "new-file");
-    fileName.innerHTML = `File Name: ${file.name}`;
-    fileType.innerHTML = `Type: ${file.type}`;
-    ul.appendChild(fileName);
-    ul.appendChild(fileType);
-    list.appendChild(ul);
-};
-
-const drop = (event) => {
-    console.log(`Something dropped here.`);
+const fileDrop = (event) => {
+    event.stopPropagation();
     event.preventDefault();
-    const list = document.getElementById('list-files');
-    const filesNumber = document.getElementById('files-number');
-
-    if (event.dataTransfer.items) {
-        filesNumber.innerHTML = event.dataTransfer.items.length;
-        for(let i = 0; i < event.dataTransfer.items.length; i++) {
-            if (event.dataTransfer.items[i].kind === 'file') {
-                const file = event.dataTransfer.items[i].getAsFile();
-                listFiles(file, list);
-                console.log(`File ${i} Name: ${file.name}`);
+    const files = event.dataTransfer.files;
+    for (let i of files) {
+        if (i.type == "text/plain") {
+            const readFile = new FileReader();
+            readFile.readAsText(i);
+            readFile.onload = (event) => {
+                fileData.innerHTML = `File: "${i.name}" - Type: ${i.type} - Content: ${event.target.result}`;
             }
+        } else {
+            fileData.innerHTML = `Sorry, only .txt files for now.`;
         }
-    } else {
-        filesNumber.innerHTML = event.dataTransfer.files.length;
-        for (let i = 0; i < event.dataTransfer.files.length; i++) {
-            console.log(`File ${i} Name: ${event.dataTransfer.files[i].name}`);
-            listFiles(event.dataTransfer.files[i], list);
-        }
-    } 
-    removeData(event);
+    }
 };
 
-const dragOver = (event) => {
-    console.log(`Something is about to drop here.`); 
+const dragOver = (event) => { 
+    event.stopPropagation();
     event.preventDefault();
 };
 
-files.addEventListener('drop', drop);
-files.addEventListener('dragover', dragOver);
+filesArea.addEventListener('drop', fileDrop);
+filesArea.addEventListener('dragover', dragOver);
